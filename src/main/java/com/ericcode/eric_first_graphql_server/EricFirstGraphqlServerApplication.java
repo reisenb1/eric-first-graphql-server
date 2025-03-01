c22041fff3bf419b4db0246393dc60246475d557
Eric Reisenbuckler <ericreisenbuckler@Erics-MacBook-Pro.local>
2025-03-01 14:44:42 -0500

    Starting point of project.

---
+++ src/main/java/com/ericcode/eric_first_graphql_server/EricFirstGraphqlServerApplication.java
@@ -0,0 +1,13 @@
+package com.ericcode.eric_first_graphql_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EricFirstGraphqlServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EricFirstGraphqlServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ericcode/eric_first_graphql_server/EricFirstGraphqlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericcode.eric_first_graphql_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EricFirstGraphqlServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
